308e0799e56d26ec7dcef9606eb006fe01b3a527
leiboT <superviho1@gmail.com>
2023-07-11 01:30:58 +0800

    ci: vue test

--- jest.config.js
+++ jest.config.js
@@ -13,10 +13,12 @@ module.exports = {
   transform: {
     '.+\\.(css|styl|less|sass|scss|jpg|jpeg|png|svg|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|avif)$':
     require.resolve('jest-transform-stub'),
+    '^.+\\.vue$': '@vue/vue2-jest',
     '^.+\\.(t|j)sx?$': ['babel-jest', {
       presets: [
         ['@babel/preset-env', {targets: {node: 'current'}}],
-        '@babel/preset-typescript'
+        '@babel/preset-typescript',
+        '@vue/babel-preset-jsx'
       ]
     }]
   },
